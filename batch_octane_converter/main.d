da0c137cd7012c5d3d97493c51c63ee170f9b169
Danni Coy <danni.coy@gmail.com>
2016-04-07 16:39:50 +1000

    wokr

--- batch_octane_converter/main.d
+++ batch_octane_converter/main.d
@@ -6,6 +6,8 @@ import std.path;
 import std.range : array,take;
 import std.algorithm : find, canFind;
 import std.conv : to;
+import std.process : executeShell;
+import std.string : replace;
 
 void main(string[] args)
 {
@@ -17,13 +19,14 @@ void main(string[] args)
 	{
 		auto max_executables = dirEntries("C:\\Program Files\\Autodesk\\","*3dsmax.exe",SpanMode.depth);
 	}
-	auto max2014 = find(max_executables,"2014");
-	assert(max2014.length > 0,"failed to find 2014\n");
 
-	foreach(entry; max_executables)
+	string[string] exes;
+	foreach(exe; max_executables)
 	{
-		writeln(entry);
+		if (canFind(exe,"2014")) exes["2014"] = exe;
+		else if (canFind(exe,"2015")) exes["2015"] = exe;
 	}
+
 	version(linux)
 	{
 		auto import_script = "/media/danni/network/resources/Temp/danni/OctaneImporter/importer.ms";
@@ -39,7 +42,21 @@ void main(string[] args)
 
 	foreach (max_file; max_files)
 	{
-
+		auto mat_file = replace(max_file, ".max", ".mat");
+		if (!mat_file.exists)
+		{
+			string import_cmd = format("\"%s\" \"%s\" -u \"%s\"", exes["2014"], max_file, import_script);
+			executeShell(import_cmd);	
+		}
+	}
+	foreach (max_file; max_files)
+	{
+		auto mat_file = replace(max_file, ".max", ".mat");
+		if (mat_file.exists)
+		{
+			string export_cmd = format("\"%s\" \"%s\" -u \"%s\"",exes["2015"], max_file, export_script);
+			executeShell(export_cmd);
+		}
 	}
 
 	// Prints "Hello World" string in console
